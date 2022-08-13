a93041cb2f72c3f909af5b09ee9339b66975a64c
ExMaterial <3071969304@qq.com>
2022-08-13 10:59:47 +0800

    全部路由完成以及部分组件封装

---
+++ src/components/RightCpn/DownloadApp.vue
@@ -0,0 +1,43 @@
+<template>
+    <a class="download" href="https://juejin.cn/app?utm_source=jj_nav" target="_blank">
+        <div class="left">
+            <img src="../../assets/download.jpg" alt="" class="image">
+        </div>
+        <div class="right">
+            <h3>下载稀土掘金App</h3>
+            <p>一个帮助开发者成长的社区</p>
+        </div>
+    </a>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="less">
+.download {
+    box-sizing: border-box;
+    display: flex;
+    padding: 1rem;
+    width: 240px;
+    height: 75px;
+    cursor: pointer;
+    background-color: white;
+
+    .image {
+        margin-right: 1rem;
+    }
+
+    .right {
+        h3 {
+            color: black;
+            font-size: 14px;
+        }
+
+        p {
+            margin-top: 15px;
+            color: #71777c;
+        }
+    }
+}
+</style>
